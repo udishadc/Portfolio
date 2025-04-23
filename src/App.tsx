0e03d5601df07d7d18e2be22db0f2567aaccc6d9
udishadc <udishaduttachowdhury@gmail.com>
2025-04-23 17:23:33 -0400

    Add basic content and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import { Container, CssBaseline, ThemeProvider, createTheme, Box, Typography } from '@mui/material';
 import Navbar from './components/Navbar';
 
 const theme = createTheme({
@@ -10,16 +10,26 @@ const theme = createTheme({
   },
 });
 
-const App: React.FC = () => {
+function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Navbar />
       <Container>
-        <h1>Welcome to My Portfolio</h1>
+        <Box sx={{ mt: 8, textAlign: 'center' }}>
+          <Typography variant="h2" component="h1" gutterBottom>
+            Welcome to My Portfolio
+          </Typography>
+          <Typography variant="h5" component="h2" gutterBottom>
+            I'm a Full Stack Developer
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            This portfolio showcases my projects and skills in web development.
+          </Typography>
+        </Box>
       </Container>
     </ThemeProvider>
   );
-};
+}
 
 export default App;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Portfolio
+          Udisha's Portfolio
         </Typography>
         <Box>
           <Button color="inherit">Home</Button>
